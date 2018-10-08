9557cd3a71f6b96d61e5c85a4b990b55ab06d7eb
efdssdf <efdssdf@gmail.com>
2018-10-08 13:09:01 +0800

    'fix'

--- tuitui_weichat/routes/weichat.js
+++ tuitui_weichat/routes/weichat.js
@@ -131,7 +131,7 @@ async function validate(req, res) {
     if (!config) {
         config = await ConfigModel.find({code: req.params.code})
         config = config[0]
-        console.log(config,'--------------------------config')
+        console.log(config, '--------------------------config')
         await mem.set("configure_" + req.params.code, config, 30 * 24 * 3600)
     }
     var token = config.token;
@@ -204,8 +204,9 @@ function getUserInfo(openid, config, message, request, w_req, w_res, next) {
 
 async function reply(code, res, type, param, openid) {
     var reply = await mem.get("reply_" + code + "_" + param);
+    console.log(reply, '------------------------reply1')
 
-    if (!reply) {
+    if (!reply || reply.length <= 0) {
         if (type == 0) {
             reply = await ReplyModel.find({code: code, type: type, text: param})
         } else if (type == 1) {
@@ -213,11 +214,11 @@ async function reply(code, res, type, param, openid) {
         } else if (type == 2) {
             reply = await ReplyModel.find({code: code, type: type})
         }
-        console.log(reply,'------------------------reply')
+        console.log(reply, '------------------------reply2')
         if (reply[0].replyType == 0) {
-            reply = {type:0,msg:reply[0].msgId}
+            reply = {type: 0, msg: reply[0].msgId}
         } else if (reply[0].replyType == 1) {
-            reply = {type:1,msg:reply[0].media}
+            reply = {type: 1, msg: reply[0].media}
         }
         await mem.set("reply_" + code + "_" + param, reply, 30 * 24 * 3600)
     }
