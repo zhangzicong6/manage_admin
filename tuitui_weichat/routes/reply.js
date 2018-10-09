2eaff40b4bcf1608e9e6fb869f351b66faf1ce2e
efdssdf <efdssdf@gmail.com>
2018-10-09 17:57:20 +0800

    'fix'

--- tuitui_weichat/routes/reply.js
+++ tuitui_weichat/routes/reply.js
@@ -38,7 +38,6 @@ router.post('/create', async(req, res, next) => {
                         mediaId: result.media_id
                     }
                 }
-                console.log(media,'-----------------media')
                 let data = {
                     code: req.body.code,
                     type: req.body.type,
