eade9d679423354ee6c8c876d128061b8f7c8033
unknown <weblixin@163.com>
2018-12-05 17:28:56 +0800

    ok

--- tuitui_weichat/routes/novelTransfer.js
+++ tuitui_weichat/routes/novelTransfer.js
@@ -3,22 +3,22 @@ var router = express.Router();
 var NovelTransferModel = require('../model/NovelTransfer');
 
 router.post('/', async(req, res, next) => {
-  await const docs = NovelTransferModel.findOne({url: req.body.url });
+  const docs = await NovelTransferModel.findOne({url: req.body.url });
   if(docs) {
     res.send({data: 'http://erji.nyzda.top/novel_transfer/' + docs._id})
   } else {
-    await const message = NovelTransferModel.create({ url: req.body.url });
+    const message = await NovelTransferModel.create({ url: req.body.url });
     res.send({data: 'http://erji.nyzda.top/novel_transfer/' + message._id})
   }
 })
 
 router.get('/', async(req, res, next) => {
-  await const docs = NovelTransferModel.find();
+  const docs = await NovelTransferModel.find();
   res.send({data: docs})
 })
 
 router.get('/find_one', async(req, res, next) => {
-  await const docs = NovelTransferModel.find({$or:[{ url: req.query.searchUrl },{ replaceUrl: req.query.searchUrl }]});
+  const docs = await NovelTransferModel.find({$or:[{ url: req.query.searchUrl },{ replaceUrl: req.query.searchUrl }]});
   if(docs) {
     res.send({success: '查询成功', data: docs})
   } else {
@@ -29,7 +29,7 @@ router.get('/find_one', async(req, res, next) => {
 // 修改replaceUrl
 router.post('/update', async(req, res, next) => {
   let id = req.body._id, message = { replaceUrl: req.body.replaceUrl };
-  await const docs = NovelTransferModel.findByIdAndUpdate(id, message)
+  const docs = await NovelTransferModel.findByIdAndUpdate(id, message)
   res.send({success: '修改成功', data: docs})
 })
 
