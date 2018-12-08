6bb62fb955992dbf3aa49e4ab8bc46686ef09dba
efdssdf <efdssdf@gmail.com>
2018-12-08 17:18:48 +0800

    'tag'

--- tuitui_weichat/routes/message.js
+++ tuitui_weichat/routes/message.js
@@ -4,8 +4,9 @@ var MessageModel = require('../model/Message');
 var ConfigModel = require('../model/Config');
 var send = require('../script/send_message');
 var sendUser = require('../script/send_user_message');
+var sendTag = require('../script/send_tag_message');
 
-router.get('/', async(req, res, next) => {
+router.get('/', async (req, res, next) => {
     let messages = await MessageModel.find().limit(20).sort({_id: -1});
     for (let i = 0; i < messages.length; i++) {
         let d = new Date(messages[i].timing_time);
@@ -36,13 +37,13 @@ router.get('/', async(req, res, next) => {
     res.send({messages: messages})
 })
 
-router.get('/get_code', async(req, res, next) => {
+router.get('/get_code', async (req, res, next) => {
     let doc = await ConfigModel.find()
     res.send({data: doc})
 })
 
 
-router.post('/create', async(req, res, next) => {
+router.post('/create', async (req, res, next) => {
     var message = {
         codes: req.body.codes,
         sex: req.body.sex,
@@ -64,7 +65,7 @@ router.post('/create', async(req, res, next) => {
 
 })
 
-router.post('/update', async(req, res, next) => {
+router.post('/update', async (req, res, next) => {
     var id = req.body.id;
     var message = {
         codes: req.body.codes,
@@ -86,17 +87,17 @@ router.post('/update', async(req, res, next) => {
     }
 })
 
-router.get('/delete', async(req, res, next) => {
+router.get('/delete', async (req, res, next) => {
     var id = req.query.id;
     var doc = await MessageModel.findByIdAndRemove(id)
-    if (doc){
+    if (doc) {
         res.send({success: '删除成功', data: doc})
-    } else{
+    } else {
         res.send({err: '删除失败'})
     }
 })
 
-router.get('/send', async(req, res, next) => {
+router.get('/send', async (req, res, next) => {
     var id = req.query.id;
     var take_over = req.query.take_over;
     console.log('take_over-------------' + take_over)
@@ -113,14 +114,8 @@ router.get('/sendMsg', async (req, res, next) => {
     var id = req.query.id;
     var tagId = req.query.tagId;
     var mediaId = req.query.mediaId;
-    console.log('take_over-------------' + take_over)
-    if (take_over) {
-        sendUser.get_message(id, tagId, mediaId);
-        res.send({success: '发送成功'})
-    } else {
-        send.get_message(id, tagId, mediaId);
-        res.send({success: '发送成功'})
-    }
+    sendTag.get_message(id, tagId, mediaId);
+    res.send({success: '发送成功'})
 })
 
 module.exports = router
