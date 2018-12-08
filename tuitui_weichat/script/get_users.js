56663a3c9995844e531d9d642d80aadbd9c171f0
efdssdf <efdssdf@gmail.com>
2018-12-08 19:34:14 +0800

    'tag'

--- tuitui_weichat/script/get_users.js
+++ tuitui_weichat/script/get_users.js
@@ -8,14 +8,28 @@ var UserTagModel = require('../model/UserTag')
 
 function getUserByCode(code) {
     UserconfModel.remove({code: code}, async function (err, doc) {
+        let client = await wechat_util.getClient(code)
+        let tagId0;
+        let tagId1;
+        let tagId2;
+        client.createTag("未知", async function (err, data) {
+            await UserTagModel.create({id: data.tag.id, name: "未知", code: code})
+            tagId0 = data.tag.id
+        })
+        client.createTag("男", async function (err, data) {
+            await UserTagModel.create({id: data.tag.id, name: "男", code: code})
+            tagId1 = data.tag.id
+        })
+        client.createTag("女", async function (err, data) {
+            await UserTagModel.create({id: data.tag.id, name: "女", code: code})
+            tagId2 = data.tag.id
+        })
         get_users(code, null, function () {
-            get_user(null, code, function () {
-                tag(code, async function () {
-                    await OpenidModel.remove({code: code})
-                    await mem.set("jieguan_" + code, 1, 30 * 24 * 3600)
-                    await ConfigModel.update({code: code}, {status: 1})
-                    console.log('jieguan end')
-                })
+            get_user(null, code, tagId0, tagId1, tagId2, async function () {
+                await OpenidModel.remove({code: code})
+                await mem.set("jieguan_" + code, 1, 30 * 24 * 3600)
+                await ConfigModel.update({code: code}, {status: 1})
+                console.log('jieguan end')
             });
         })
     });
@@ -89,16 +103,16 @@ async function get_users(code, openid, callback) {
     }
 }
 
-async function get_user(_id, code, back) {
+async function get_user(_id, code, tagId0, tagId1, tagId2, back) {
     if (code) {
-        update_user(_id, code, get_user, back);
+        update_user(_id, code, tagId0, tagId1, tagId2, get_user, back);
     } else {
         console.log('update_user end');
         back(null);
     }
 }
 
-function update_user(_id, code, next, back) {
+function update_user(_id, code, tagId0, tagId1, tagId2, next, back) {
     OpenidModel.fetch(_id, code, async function (error, users) {
         var user_arr = [];
         users.forEach(function (user) {
@@ -120,7 +134,11 @@ function update_user(_id, code, next, back) {
                 }
                 if (data && data.user_info_list) {
                     let userArr = []
+                    let arr0 = []
+                    let arr1 = []
+                    let arr2 = []
                     async.eachLimit(data.user_info_list, 50, function (info, callback) {
+                        console.log(info)
                         if (info.nickname) {
                             userArr.push({
                                 code: info.code,
@@ -131,11 +149,27 @@ function update_user(_id, code, next, back) {
                                 sign: 1
                             })
                         }
+                        if (info.sex == "1") {
+                            arr1.push(info.openid)
+                        } else if (info.sex == "2") {
+                            arr2.push(info.openid)
+                        } else {
+                            arr0.push(info.openid)
+                        }
                         callback(null)
                     }, function (error) {
                         if (error) {
                             console.log(error, '--------------error')
                         }
+                        client.membersBatchtagging(tagId0, arr0, function (error, res) {
+                            console.log(res)
+                        })
+                        client.membersBatchtagging(tagId1, arr1, function (error, res) {
+                            console.log(res)
+                        })
+                        client.membersBatchtagging(tagId2, arr2, function (error, res) {
+                            console.log(res)
+                        })
                         UserconfModel.insertMany(userArr, async function (error, docs) {
                             if (error) {
                                 console.log('------insertMany error--------');
@@ -143,7 +177,7 @@ function update_user(_id, code, next, back) {
                                 console.log('------------------------------');
                             }
                             if (users.length == 50) {
-                                next(users[49]._id, code, back);
+                                next(users[49]._id, code, tagId0, tagId1, tagId2, back);
                             } else {
                                 next(null, null, back)
                             }
@@ -152,53 +186,6 @@ function update_user(_id, code, next, back) {
                 }
             })
         }
-    })
-}
-
-async function tag(code, back) {
-    let client = await wechat_util.getClient(code)
-    async.waterfall([
-        function (callback) {
-            client.createTag("男", async function (err, data) {
-                await UserTagModel.create({id: data.tag.id, name: "男", code: code})
-                let arr1 = []
-                UserconfModel.find({sex: "1"}, function (err, res) {
-                    for (d of res) {
-                        arr1.push(d.openid)
-                    }
-                    client.membersBatchtagging(data.tag.id, arr1, function (error, res) {
-                        callback(null)
-                    })
-                })
-            })
-        }, function (callback) {
-            client.createTag("女", async function (err, data) {
-                await UserTagModel.create({id: data.tag.id, name: "女", code: code})
-                let arr2 = []
-                UserconfModel.find({sex: "2"}, function (err, res) {
-                    for (d of res) {
-                        arr2.push(d.openid)
-                    }
-                    client.membersBatchtagging(data.tag.id, arr2, function (error, res) {
-                        callback(null)
-                    })
-                })
-            })
-        }, function (callback) {
-            client.createTag("未知", async function (err, data) {
-                await UserTagModel.create({id: data.tag.id, name: "未知", code: code})
-                let arr3 = []
-                UserconfModel.find({sex: "0"}, function (err, res) {
-                    for (d of res) {
-                        arr3.push(d.openid)
-                    }
-                    client.membersBatchtagging(data.tag.id, arr3, function (error, res) {
-                        callback(null)
-                    })
-                })
-            })
-        }], function (error) {
-        back(null)
     })
 }
 
