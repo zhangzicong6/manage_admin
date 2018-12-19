8ef94344683dc042a89fd2e99a8cce8e3f93749c
Administrator <lionel@buzzy.cc>
2018-12-19 18:01:26 +0800

    执行

--- tuitui_weichat/script/clear_mem.js
+++ tuitui_weichat/script/clear_mem.js
@@ -1,5 +1,9 @@
 var mem = require('../util/mem.js');
+var wechat_util = require('../util/get_weichat_client.js')
 
-mem.set("configure_" +52 , '', 10,function (res) {
-	console.log(res)
-})
+function test() {
+	await mem.set("configure_" +52 , '', 10)
+	await wechat_util.getClient(52)
+}
+
+test()
