1dfd05c9d369cd8da910fe9ffbf9a1a71a553391
efdssdf <efdssdf@gmail.com>
2018-12-20 11:16:41 +0800

    'log'

--- tuitui_weichat/routes/conf.js
+++ tuitui_weichat/routes/conf.js
@@ -69,11 +69,10 @@ router.get('/reset', async(req, res, next) => {
 
 router.get('/jieguan', async(req, res, next) => {
     let code = req.query.code
-    console.log(code, '--------------code')
     let jieguan = await mem.get("jieguan_" + code)
     // if(!jieguan){
     await ConfigModel.findOneAndUpdate({code: code}, {status: -1})
-    let cmdStr = 'node ' + __dirname + '/../script/get_user ' + code
+    let cmdStr = 'node /home/work/tuitui_pro/tuitui_weichat/script/get_user ' + code
     console.log(cmdStr)
     exec(cmdStr, function (err, stdout, stderr) {
         res.send({success: '设置接管成功'})
