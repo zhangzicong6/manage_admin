a19a45b690ebcf3c25ba381c00897bb56037a2f7
Administrator <lionel@buzzy.cc>
2018-12-20 12:59:12 +0800

    ok

--- tuitui_weichat/script/update.js
+++ tuitui_weichat/script/update.js
@@ -1,4 +1,4 @@
-var ConfigModel = require('../model/Config');
+/*var ConfigModel = require('../model/Config');
 
 async function delTag(code) {
     await ConfigModel.update({code:code},{status:1})
@@ -6,7 +6,17 @@ async function delTag(code) {
 
 for (let i = 54; i <= 62; i++) {
     delTag(i)
-}
+}*/
 
+
+var program = require('commander');
+
+program.version('1.0.0')
+.option('-C , --code <n>','add a code for wechat ',parseInt)
+.parse(process.argv);
+
+if(program.code){
+	
+}
 
 
