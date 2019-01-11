388f6571be13f717772075a11399d85e9312305b
efdssdf <efdssdf@gmail.com>
2019-01-11 17:07:50 +0800

    'script'

--- tuitui_weichat/script/test_quota.js
+++ tuitui_weichat/script/test_quota.js
@@ -1,10 +1,13 @@
 var ConfigModel = require('../model/Config');
 var wechat_util = require('../util/get_weichat_client.js')
 
-async function test(appid, code) {
+async function test(code) {
     let client = await wechat_util.getClient(code)
+    let conf = await ConfigModel.findOne({code: code})
+    let appid = conf.appid
     client.clearQuota(appid, function (err, data) {
+        console.log(err, data, '------------------------------')
         console.log('clearQuota end')
     })
 }
-test('wxc91ed548a6a8afd6', 149)
+test(149)
