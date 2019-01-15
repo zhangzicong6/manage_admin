f443bb1e0dbf9c9691e1b33ed1e8edeb56576c98
unknown <weblixin@163.com>
2019-01-15 15:48:31 +0800

    ok

--- tuitui_weichat/routes/material.js
+++ tuitui_weichat/routes/material.js
@@ -63,7 +63,8 @@ router.get('/sendMsg', async (req, res, next) => {
       delete result._id;
       result.tagId = tagId
       result.msg_id = docs.msg_id
-      result.update_time = Date.now() * 1000
+      result.update_time = Date.now()
+      console.log(result)
       let message = await MsgHistoryModel.create(result)
       res.send({
         success: '发送成功', result: result, docs: docs, message: message
