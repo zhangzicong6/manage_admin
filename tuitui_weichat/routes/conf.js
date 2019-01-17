3ba7372c7791845ac5b11c138d8632d613b06f54
unknown <weblixin@163.com>
2019-01-17 15:06:27 +0800

    ok

--- tuitui_weichat/routes/conf.js
+++ tuitui_weichat/routes/conf.js
@@ -14,9 +14,7 @@ router.get('/', async(req, res, next) => {
 
 router.get('/find_one', async(req, res, next) => {
   let reg = new RegExp(req.query.name);
-  // let doc = await ConfigModel.find({name: {$regex: reg}})
-  console.log(req.query.name)
-  let doc = await ConfigModel.find({name: req.query.name})
+  let doc = await ConfigModel.find({name: {$regex: reg}})
   res.send({data: doc})
 })
 
