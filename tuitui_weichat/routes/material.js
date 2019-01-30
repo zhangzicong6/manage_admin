8ac42597a36121cb2db8bcc1eaf5507e6efb739a
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-01-30 18:47:32 +0800

    ok

--- tuitui_weichat/routes/material.js
+++ tuitui_weichat/routes/material.js
@@ -66,6 +66,18 @@ router.get('/send_timing', async (req, res, next) => {
   }
 })
 
+router.get('/copy', async (req, res, next) => {
+  let id = req.query.id;
+  let result = await MaterialModel.findById(id)
+  if(result) {
+    result = result.toObject();
+    delete result._id
+    res.send({success: "复制成功", data: result})
+  } else {
+    res.send({err: "复制失败"})
+  }
+})
+
 router.get('/tag', async (req, res, next) => {
   let doc = await UserTagModel.find({
     code: req.query.code
