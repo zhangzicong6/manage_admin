0ec7d772b919ef33abe5a67066e1e6de5970c57c
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-02-01 15:43:07 +0800

    ok

--- tuitui_weichat/routes/transfer.js
+++ tuitui_weichat/routes/transfer.js
@@ -10,7 +10,7 @@ router.get('/', async(req, res, next) => {
 })
 
 router.get("/find_count", async(req, res, next) => {
-  const messages = await TransferModel.count({_id: {$lt: "5c2359c4b221222e3cc809b1"}})
+  let messages = await TransferModel.count({_id: {$lt: "5c2359c4b221222e3cc809b1"}})
   res.send(messages)
 })
 
