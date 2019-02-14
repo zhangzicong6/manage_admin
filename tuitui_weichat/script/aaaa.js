ef660111d380ed1990cffc02d4c2da375c5ee35b
efdssdf <efdssdf@gmail.com>
2019-02-14 14:34:51 +0800

    'aaaa'

--- tuitui_weichat/script/aaaa.js
+++ tuitui_weichat/script/aaaa.js
@@ -7,14 +7,17 @@ async function a(code) {
     // await ConfigModel.update({code: 199}, {status: -2})
 
     let client = await wechat_util.getClient(code)
-    client.getTags(function (err,data) {
-        console.log(data,'-----------------data')
-    })
-    // client.createTag("明星说女", async function (err, data) {
-    //     await UserTagModel.create({id: data.tag.id, name: "女", code: code})
-    //     get_tag(null, code, data.tag.id, '2', function () {
-    //         callback(null)
-    //     })
+    // client.getTags(function (err,data) {
+    //     console.log(data,'-----------------data')
     // })
+    client.createTag("明星说女", async function (err, data) {
+        await UserTagModel.create({id: data.tag.id, name: "女", code: code})
+        get_tag(null, code, data.tag.id, '2', function () {
+            // callback(null)
+            client.getTags(function (err,data) {
+                console.log(data,'-----------------data')
+            })
+        })
+    })
 }
 a(255)
