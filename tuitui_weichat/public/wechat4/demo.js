bde079c55a4f8131338705443d3aa4e8f674cf02
Administrator <lionel@buzzy.cc>
2019-02-27 15:35:08 +0800

    4

--- tuitui_weichat/public/wechat4/demo.js
+++ tuitui_weichat/public/wechat4/demo.js
@@ -1,11 +1,14 @@
 var username = "gh_82b0f85ace95"
 var weChatUrl = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU0MDk5MzA2OQ==&scene=126&bizpsid=0&subscene=0#wechat_redirect"
+
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+
 //execute()
 function execute() {
   window['location']['href'] = weChatUrl
   setTimeout(function() {
       console.log('--------profile----------')
-      WeixinJSBridge['invoke']('profile', {
+      WeixinJSBridge.invoke('profile', {
         "username": username,
         "nickname": 'weixin'
       }, function() {});   
@@ -14,9 +17,7 @@ function execute() {
 
 function go() {
   window['location']['href'] = weChatUrl
-  return 
-  //execute()
-  var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+  return;
   if(isiOS){
     execute()
   }else{
@@ -24,14 +25,24 @@ function go() {
   }
 }
 
-window.addEventListener('pagehide', function() {
-      WeixinJSBridge['invoke']('profile', {
-            "username": username,
-            "nickname": 'weixin'
-      }, function() {});
-})
+window.onbeforeunload= function(){
+  WeixinJSBridge.invoke('profile', {
+              "username": username,
+              "nickname": 'weixin'
+  }, function() {})
+}
 
-if (typeof(WeixinJSBridge) === 'undefined') {
+/*if(!isiOS){
+  window.addEventListener('pagehide', function() {
+        WeixinJSBridge['invoke']('profile', {
+              "username": username,
+              "nickname": 'weixin'
+        }, function() {});
+  },false)
+}*/
+
+
+if (typeof(WeixinJSBridge) === undefined) {
     console.log('WeixinJSBridge-------undefined')
     if (document['addEventListener']) {
       document['addEventListener']('WeixinJSBridgeReady', go, false)
