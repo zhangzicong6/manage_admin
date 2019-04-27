dfb2c11dca4c1b4f0ce9ca31a07ac8da7e8c3db5
efdssdf <efdssdf@gmail.com>
2019-04-27 16:34:39 +0800

    'aa'

--- tuitui_weichat/script/aaaa.js
+++ tuitui_weichat/script/aaaa.js
@@ -17,23 +17,23 @@ async function a() {
     // client.deleteTag(100, function (error, res) {
     //     console.log(res)
     // })
-    client.createTag("明星说未知", async function (err, data) {
-        await UserTagModel.create({id: data.tag.id, name: "未知", code: code, sex: '0'})
-    })
-    client.createTag("明星说男", async function (err, data) {
-        await UserTagModel.create({id: data.tag.id, name: "男", code: code, sex: '1'})
-    })
-    client.createTag("明星说女", async function (err, data) {
-        await UserTagModel.create({id: data.tag.id, name: "女", code: code, sex: '2'})
-    })
-
-        // let openid = await OpenidModel.count({code:code})
-    // console.log(openid,'--------------openid')
-    // let user = await UserconfModel.count({code:code})
-    // console.log(user,'-----------------user')
-    // client.getTags(function (err,data) {
-    //     console.log(data,'-----------------aaa')
+    // client.createTag("明星说未知", async function (err, data) {
+    //     await UserTagModel.create({id: data.tag.id, name: "未知", code: code, sex: '0'})
     // })
+    // client.createTag("明星说男", async function (err, data) {
+    //     await UserTagModel.create({id: data.tag.id, name: "男", code: code, sex: '1'})
+    // })
+    // client.createTag("明星说女", async function (err, data) {
+    //     await UserTagModel.create({id: data.tag.id, name: "女", code: code, sex: '2'})
+    // })
+
+    let openid = await OpenidModel.count({code:code})
+    console.log(openid,'--------------openid')
+    let user = await UserconfModel.count({code:code})
+    console.log(user,'-----------------user')
+    client.getTags(function (err,data) {
+        console.log(data,'-----------------aaa')
+    })
     // client.createTag("明星说女", async function (err, data) {
     //     console.log(data, '-----------------data')
     //     await UserTagModel.create({id: data.tag.id, name: "女", code: code})
